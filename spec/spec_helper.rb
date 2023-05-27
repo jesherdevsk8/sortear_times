e6ac75e061bd1568109d65e65646a7dba437b719
Jesher Minelli <jesherdevsk8@gmail.com>
2023-05-27 14:03:07 -0300

    test: initialize rspec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+ENV['RACK_ENV'] = 'test'
+
+require './config/environment'
+require 'simplecov'
+
+SimpleCov.start do
+  add_group 'App', 'app'
+
+  add_filter 'spec'
+end
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+end
