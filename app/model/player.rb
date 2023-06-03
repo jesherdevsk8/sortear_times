9815d1a36e6543d2b78f9a96bb6f66d0b374df00
Jesher Minelli <jesherdevsk8@gmail.com>
2023-06-03 18:23:54 -0300

    test: fix tests

--- app/model/player.rb
+++ app/model/player.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
 class Player < ActiveRecord::Base
-  validates_presence_of :name
+  validates :name, presence: true
+  validates :nickname, uniqueness: true, presence: true
+  validates :status, presence: true
+  validates :position, presence: true
+
+  # TODO: try add enums
+  # enum status: { 'titular': 0, 'reserva': 1 }
+  # enum position: { goleiro: 0, linha: 1 }
 end
