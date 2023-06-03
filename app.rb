aef21b3ee16b96ac55b4b3f3f305b41ce4869226
Jesher Minelli <jesherdevsk8@gmail.com>
2023-06-03 20:33:01 -0300

    feat: add endpoint to raffle team

--- app.rb
+++ app.rb
@@ -6,6 +6,7 @@ require 'sinatra/activerecord'
 require 'dotenv/load'
 
 require './app/adapters/controllers/players_controller'
+require './app/adapters/controllers/raffle_controller'
 
 set :database, { adapter: 'sqlite3', database: ENV.fetch('DB_PATH', nil) }
 # set :database, { adapter: 'sqlite3', database: ENV.fetch('DB_PATH_TEST', nil) }
@@ -16,4 +17,8 @@ end
 
 get '/players' do
   { result: PlayersController.index }.to_json
+end
+
+get '/raffle' do
+  { result: PlayersController.raffle }.to_json
 end
