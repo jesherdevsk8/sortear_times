f82bca7eb2cf9cc3f8192a58744c93de1bdfd2d0
Jesher Minelli <jesherdevsk8@gmail.com>
2023-06-10 20:28:46 -0300

    feat: add database enviroment like rails

--- app.rb
+++ app.rb
@@ -8,8 +8,6 @@ require 'dotenv/load'
 require './app/adapters/controllers/players_controller'
 require './app/adapters/controllers/raffles_controller'
 
-set :database, { adapter: 'sqlite3', database: ENV.fetch('DB_PATH', nil) }
-# TODO: add enviroments like rails aplication [dev, test, prod]
 # Uncomment if you want to check data inside db-test
 # set :database, { adapter: 'sqlite3', database: ENV.fetch('DB_PATH_TEST', nil) }
 
