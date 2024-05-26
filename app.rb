3099e9f5b8bab3d1389e9dc442e51029be45de18
Jesher Minelli <jesherdevsk8@gmail.com>
2024-05-26 15:21:16 -0300

    add support to layouts erb on server

--- app.rb
+++ app.rb
@@ -10,6 +10,18 @@ require './app/adapters/controllers/raffles_controller'
 
 # Uncomment if you want to check data inside db-test
 # set :database, { adapter: 'sqlite3', database: ENV.fetch('DB_PATH_TEST', nil) }
+set :views, File.expand_path('app/views', __dir__)
+
+# Root route
+get '/' do
+  @players = PlayersController.show
+  erb :'players/index'
+end
+
+get '/randomize' do
+  @teams = RafflesController.raffle.flatten
+  erb :'players/teams'
+end
 
 post '/players' do
   { result: PlayersController.create(params) }.to_json
--- app/adapters/controllers/players_controller.rb
+++ app/adapters/controllers/players_controller.rb
@@ -1,14 +1,19 @@
 # frozen_string_literal: true
 
+require './app/usecases/players/index'
 require './app/usecases/players/show'
 require './app/usecases/players/create'
 
 class PlayersController
   def self.index
-    Players::Show.call
+    Players::Index.call
   end
 
   def self.create(params)
     Players::Create.new(params:).call
+  end
+
+  def self.show
+    Players::Show.call
   end
 end
